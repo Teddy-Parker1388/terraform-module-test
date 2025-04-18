2202924e5520226c87da1db4e3e279e693ba918f
Teddy Parker <57333590+Teddy-Parker1388@users.noreply.github.com>
2025-04-18 13:19:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "aws_instance" "my_instance" {
-    ami = "ami-0b72821e2f351e396"
-    instance_type = var.instance_type
-tags = {
-Name = "Server Two"
-}
+output "mytest"{
+    value = "test"
+
 }
